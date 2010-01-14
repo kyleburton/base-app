312a2ff751eb6659ddc73003ca7e0ab3f42218b9
Kyle Burton <kyle.burton@algorithmics.com>
2010-01-14 08:14:35 -0500

    less verbose, default help text

--- lib/base_app.rb
+++ lib/base_app.rb
@@ -11,16 +11,17 @@ class BaseApp
   end
 
   def command_line_arguments
-    [['v','verbose','Be more verbose']]
+    [['v','verbose','Be more verbose.'],
+     ['h', 'help',   'Request help.']]
   end
 
   def construct_opts
     opts = OptionParser.new do |opts|
       command_line_arguments.each do |argspec|
-        short, long, description, required = argspec
+        short, long, description, required, default_value = argspec
         param_name = long.gsub '=', ''
         @required_opts << param_name if required
-        create_setter(long)
+        create_getter_setter(long)
         opts.on("-#{short}", "--#{long}", description) do |val|
           @options[param_name] = val
           setter = param_name.gsub("-", "_") + "="
@@ -30,8 +31,8 @@ class BaseApp
     end
   end
 
-  def create_setter(name)
-    name = name.gsub("-", "_").gsub("=", "")
+  def create_getter_setter(name)
+    name = name.gsub("-", "_").gsub(/=.?/, "")
     self.class.send(:attr_accessor, name) unless self.respond_to?(name)
   end
 
@@ -45,19 +46,49 @@ class BaseApp
     end
   end
 
+  # file util helpers
+  def exists? file
+    File.exists? file
+  end
+
+  def mkdir dir
+    FileUtils.mkdir(dir) unless exists?(dir)
+  end
+
+  def help_text_firstline
+    "#$0: \n"
+  end
+
+  def help_text
+    text = help_text_firstline
+    command_line_arguments.each do |argspec|
+      short, long, description, required, default_value = argspec
+      short_text = (long =~ /=/) ? "#{short}=s" : short
+      text << sprintf("  -%-3s | --%-20s  %s%s\n", short_text, long, description, (required ? "(required)" : ""))
+    end
+    text
+  end
+
   def self.main
     app = self.new
     app.parse_command_line_options
+
+    if app.help
+      $stderr.print app.help_text
+      exit app.status
+    end
+
     max_width = app.command_line_arguments.map {|x| x[1].length}.max
     if app.verbose
       $stderr.puts "#$0 Parameters:"
       app.command_line_arguments.each do |opt_spec|
         short, arg, descr = opt_spec
-        option = arg.gsub(/=$/,'').gsub('-','_')
-        $stderr.printf( "  %*s : %s\n", max_width, arg.gsub('=',''), app.send(option.to_sym))
+        option = arg.gsub(/=.?$/,'').gsub('-','_')
+        $stderr.printf( "  %*s : %s\n", max_width, arg.gsub(/=.?$/,''), app.send(option.to_sym))
       end
       $stderr.puts ""
     end
+
     app.run
     exit app.status
   end
